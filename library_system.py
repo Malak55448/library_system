fb4f8b0da3a749d9e93857bb87af3418bd925457
Malak55448 <engelmalak5@gmail.com>
2026-03-17 21:54:09 +0300

    Add getter/setter for year with validation and implement Books class with inheritance

--- library_system.py
+++ library_system.py
@@ -1,8 +1,26 @@
-
 # My library system
 
-class Library_Sys:
+class Library_Sys_Item:
     def __init__(self, item_id, title, year):
-        self.item_id = item_id
+        self.item_id = item_id 
         self.title = title 
         self.__year = year 
+
+    # get private (self.__year)
+    def getYear(self):
+        return self.__year 
+    
+    #Setter with validation
+    def setYear(self, year):
+        if year >= 1988 and year <= 2026:
+          self.__year = year 
+        
+        else:
+            print("The year is incorrect!!")
+
+class Books(Library_Sys_Item):
+    def __init__(self, item_id, title, year, author, genre):
+        super.__init__(self, item_id, title, year)
+        self.author = author
+        self.genre = genre
+        self.__available = True 
